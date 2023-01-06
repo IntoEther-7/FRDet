5666854303a82383a84006aa87948a4227cc6893
Ether-c <1779562376@qq.com>
2023-01-06 15:50:11 +0800

    原来的pixel attention, 这个会让整个fg_attention=1, 需要修改

---
+++ models/FPNMAAttention.py
@@ -0,0 +1,69 @@
+# PROJECT: FRDet
+# PRODUCT: PyCharm
+# AUTHOR: 17795
+# TIME: 2022-12-28 11:57
+from torchvision.models.detection.anchor_utils import AnchorGenerator
+from torchvision.ops import MultiScaleRoIAlign
+
+from models.utils.MultiplyAttentionModule import MultiplyAttentionModule, PixelAttention, ChannelAttention
+import torch
+from torch import nn, Tensor
+from torchvision.models.detection.image_list import ImageList
+from torchvision.transforms import transforms
+from torch.nn import functional as F
+
+from models.utils.Inception import Inception
+
+
+class FPNMAAttention(nn.Module):
+    def __init__(self, in_channels, reduction=16, roi_align=None):
+        super(FPNMAAttention, self).__init__()
+        self.in_channels = in_channels
+        self.PA = PixelAttention(in_channels)
+        self.CA = ChannelAttention(in_channels, reduction=reduction)
+        self.roi_align: MultiScaleRoIAlign = roi_align
+
+    def forward(self, way, shot, support, query, image=None, target=None):
+        out = {}
+        fg = {}
+        loss_attention = None
+        for k in query.keys():
+            s = support[k]
+            _, c, w, h = s.shape
+            s = s.reshape(way, shot, c, w, h).mean(1)
+            q = query[k]
+            channel_attention = self.CA.forward(s)
+            q = channel_attention.mean(0).unsqueeze(0) * q
+            pixel_attention = F.softmax(self.PA.forward(q), dim=1)
+            fg_attention = pixel_attention[:, :1, :, :]
+            fg[k] = fg_attention
+            out[k] = q * fg_attention
+        if self.training:
+            mask = self._generate_mask(target, fg, image)
+            loss_attention = self._compute_attention_loss(mask)
+        return out, loss_attention
+
+    def forward_without_mask(self, way, shot, support, query):
+        out = {}
+        for k in query.keys():
+            s = support[k]
+            _, c, w, h = s.shape
+            s = s.reshape(way, shot, c, w, h).mean(1)
+            q = query[k]
+            n, c, w, h = q.shape
+            channel_attention = self.CA.forward(s)
+            pixel_attention = F.softmax(self.PA.forward(q), dim=1)
+            fg_attention = pixel_attention[:, :1, :, :]
+            out[k] = (q.unsqueeze(1) * channel_attention.unsqueeze(0) * fg_attention.unsqueeze(1)) \
+                .reshape(n, way * c, w, h)
+        return out
+
+    def _generate_mask(self, target, fg, image):
+        gt_bbox = [i['boxes'] for i in target]
+        mask = self.roi_align.forward(fg, gt_bbox, image.image_sizes)
+        return mask
+
+    def _compute_attention_loss(self, mask: Tensor):
+        gt_mask = torch.ones_like(mask).to(mask.device)
+        loss_attention = F.binary_cross_entropy(mask, gt_mask)
+        return loss_attention
---
+++ val_fsod_10_fpn.py
@@ -0,0 +1,71 @@
+# PROJECT: FRDet
+# PRODUCT: PyCharm
+# AUTHOR: 17795
+# TIME: 2022-11-17 18:54
+
+from torchvision.models.detection.anchor_utils import AnchorGenerator
+
+from models.FRDet import FRDet
+from utils.dataset import *
+from utils.tester import tester
+
+torch.set_printoptions(sci_mode=False)
+root = '../FRNOD/datasets/fsod'
+json_path = 'annotations/fsod_train.json'
+img_path = 'images'
+continue_weight = None
+save_root = None
+
+
+def way_shot_test(way, shot, lr, index):
+    # result_voc_r50_2way_5shot_lr2e-06_loss_weight_0
+    model = FRDet(
+        # box_predictor params
+        way, shot, roi_size=5, num_classes=way + 1,
+        # backbone
+        backbone_name='resnet50', pretrained=True,
+        returned_layers=None, trainable_layers=3,
+        # transform parameters
+        min_size=600, max_size=1000,
+        image_mean=None, image_std=None,
+        # RPN parameters
+        rpn_anchor_generator=None,
+        rpn_head=None,
+        rpn_pre_nms_top_n_train=2000, rpn_pre_nms_top_n_test=2000,
+        rpn_post_nms_top_n_train=1000, rpn_post_nms_top_n_test=1000,
+        rpn_nms_thresh=0.7,
+        rpn_fg_iou_thresh=0.7, rpn_bg_iou_thresh=0.3,
+        rpn_batch_size_per_image=256, rpn_positive_fraction=0.5,
+        rpn_score_thresh=0.0,
+        # Box parameters
+        box_roi_pool=None, box_head=None, box_predictor=None,
+        box_score_thresh=0.05, box_nms_thresh=0.7, box_detections_per_img=100,
+        box_fg_iou_thresh=0.5, box_bg_iou_thresh=0.5,
+        box_batch_size_per_image=512, box_positive_fraction=0.25,
+        bbox_reg_weights=(10., 10., 5., 5.)
+    )
+
+    tester(
+        # 基础参数
+        way=way, shot=shot, query_batch=4, is_cuda=True,
+        # 设备参数
+        random_seed=None, gpu_index=0,
+        # 数据集参数
+        root=root,
+        json_path=json_path,
+        img_path=img_path,
+        split_cats=None,
+        # 模型
+        model=model,
+        # 权重文件
+        continue_weight=continue_weight,
+        # 保存相关的参数
+        save_root=save_root)
+
+
+if __name__ == '__main__':
+    save_root = os.path.join('result',
+                             'not_flatten_model_20230105_fpn',
+                             'result_fsod_r50_5way_5shot_lr0.002')
+    continue_weight = 'FRDet_4_2300.pth'
+    way_shot_test(5, 5, 2e-03, 0)
